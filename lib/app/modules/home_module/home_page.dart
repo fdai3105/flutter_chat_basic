e2ab2333c7d65da7b29b487f1abe17da02754906
phanvanhoa2380 <hoagamer2k@gmail.com>
2021-05-26 16:48:26 +0700

    ui home

--- lib/app/modules/home_module/home_page.dart
+++ lib/app/modules/home_module/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pdteam_demo_chat/app/modules/chat_module/chat.dart';
 import 'package:pdteam_demo_chat/app/modules/home_module/home.dart';
 import 'package:pdteam_demo_chat/app/widgets/widgets.dart';
 import 'package:get/get.dart';
@@ -15,8 +16,35 @@ class HomePage extends GetView<HomeController> {
               itemCount: controller.users.length,
               itemBuilder: (context, i) {
                 final item = controller.users[i];
-                return ListTile(
-                  title: Text(item.name),
+                return GestureDetector(
+                  onTap: (){
+                    // Navigator.push(context, MaterialPageRoute(builder: (context)=> ChatPage(avatar: item.avatar!, name: item.name)));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0),
+                    child: ListTile(
+                      leading: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                        ),
+                        child: ClipOval(
+                          child: Image.network(item.avatar!,),
+                        ),
+                      ),
+                      title: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(item.name),
+                          Text(
+                            item.email,
+                            style: TextStyle(
+                              color: Colors.grey
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
                 );
               },
             );
