43611d936254a586f48ab3f62555475f33542708
Kem <badkem@Kems-MacBook-Air.local>
2021-05-26 17:25:28 +0700

    clean main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -7,20 +7,16 @@ import 'package:pdteam_demo_chat/app/modules/home_module/home_page.dart';
 import 'package:pdteam_demo_chat/app/modules/login_module/login_bindings.dart';
 import 'package:pdteam_demo_chat/app/modules/login_module/login_page.dart';
 import 'package:pdteam_demo_chat/app/routes/app_pages.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  final SharedPreferences _prefs = await SharedPreferences.getInstance();
-  var isLogin = _prefs.getBool('isLogin');
-  print(isLogin);
   runApp(
       GetMaterialApp(
         title: 'Flutter Demo',
         getPages: AppPages.pages,
-        initialBinding: isLogin != null ? HomeBinding() : LoginBinding(),
-        home: isLogin != null ? HomePage() : LoginPage(),
+        initialBinding: FirebaseAuth.instance.currentUser != null ? HomeBinding() : LoginBinding(),
+        home:  FirebaseAuth.instance.currentUser != null ? HomePage() : LoginPage(),
       )
   );
 }
