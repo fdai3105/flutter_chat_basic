ec5e2d0a8c86ae6c284ed3a404d476bc3142b122
Kem <badkem@Kems-MacBook-Air.local>
2021-05-26 16:50:35 +0700

    keep user login

--- lib/app/modules/login_module/login_controller.dart
+++ lib/app/modules/login_module/login_controller.dart
@@ -3,18 +3,23 @@ import 'package:get/get.dart';
 import 'package:pdteam_demo_chat/app/data/provider/provider.dart';
 import 'package:pdteam_demo_chat/app/data/provider/user_provider.dart';
 import 'package:pdteam_demo_chat/app/routes/app_pages.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginController extends GetxController {
   final AuthProvider provider;
+  bool? isLogin = false;
 
-  LoginController({required this.provider});
+  LoginController({required this.provider, this.isLogin});
 
   void loginWithGoogle() async {
     final user = await provider.loginWithGoogle();
+    final SharedPreferences _prefs = await SharedPreferences.getInstance();
     if (user == null) {
       Get.snackbar('Something wrong', 'Something wrong idk');
     } else {
       UserProvider(store: FirebaseFirestore.instance).saveUserToStore(user);
+      isLogin = true;
+      _prefs.setBool('isLogin', isLogin!);
       Get.toNamed(Routes.home);
     }
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pdteam_demo_chat/app/modules/home_module/home.dart';
+import 'package:pdteam_demo_chat/app/modules/home_module/home_page.dart';
 import 'package:pdteam_demo_chat/app/modules/login_module/login_bindings.dart';
 import 'package:pdteam_demo_chat/app/modules/login_module/login_page.dart';
 import 'package:pdteam_demo_chat/app/routes/app_pages.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Flutter Demo',
-      getPages: AppPages.pages,
-      initialBinding: LoginBinding(),
-      home: LoginPage(),
-    );
-  }
+  final SharedPreferences _prefs = await SharedPreferences.getInstance();
+  var isLogin = _prefs.getBool('isLogin');
+  print(isLogin);
+  runApp(
+      GetMaterialApp(
+        title: 'Flutter Demo',
+        getPages: AppPages.pages,
+        initialBinding: isLogin != null ? HomeBinding() : LoginBinding(),
+        home: isLogin != null ? HomePage() : LoginPage(),
+      )
+  );
 }
