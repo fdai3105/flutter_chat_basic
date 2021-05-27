2f22e921b922421e3720eae610c37ba092bf993a
Fi Đại <fdai3105@gmail.com>
2021-05-27 10:14:02 +0700

    update offline/online

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:pdteam_demo_chat/app/data/provider/provider.dart';
 import 'package:pdteam_demo_chat/app/modules/home_module/home.dart';
 import 'package:pdteam_demo_chat/app/modules/home_module/home_page.dart';
 import 'package:pdteam_demo_chat/app/modules/login_module/login_bindings.dart';
@@ -11,6 +12,8 @@ import 'package:pdteam_demo_chat/app/routes/app_pages.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  UserProvider().changeActive(true);
 
   runApp(GetMaterialApp(
     title: 'Flutter Demo',
